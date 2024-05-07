608164779042086f3c89f630f1a01a1d47d0054c
LilithyTv <116434991+LilithyTv@users.noreply.github.com>
2024-05-07 16:13:21 +0200

    grid
    pictures and a grid

---
+++ shop/src/components/grid/Grid.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './Grid.css'; // Corrected to match the filename exactly
+import { importAll } from './imageImporter'; // Importing the image importing function
+
+function Grid() {
+  // Importing images using the importAll function
+  const images = importAll(require.context('../../assets/images', false, /\.(png|jpe?g|svg)$/));
+
+  return (
+    <div className="grid">
+      {/* Use the imported images */}
+      {Object.keys(images).map((imageName, index) => (
+        <div key={index} className="grid-item">
+          <img src={images[imageName].default} alt={imageName} />
+        </div>
+      ))}
+      {/* Predefined grid */}
+      {Array.from({ length: 16 }, (_, index) => (
+        <div key={index} className="grid-item">{index + 1}</div>
+      ))}
+    </div>
+  );
+}
+
+export default Grid;
