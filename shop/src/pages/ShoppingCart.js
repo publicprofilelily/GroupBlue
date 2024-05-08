e81ca4e127bd95961fd33ce16343fc9c909b25b9
MinaStormcloud <164744381+MinaStormcloud@users.noreply.github.com>
2024-05-08 14:22:42 +0200

    Update ShoppingCart.js

--- shop/src/pages/ShoppingCart.js
+++ shop/src/pages/ShoppingCart.js
@@ -3,10 +3,8 @@ import React from 'react';
 export default function ShoppingCart() {
   return (
     	<div>		
-	//Font
-	<script src="https://kit.fontawesome.com/9d9796826b.js" crossorigin="anonymous"></script>
-		
-	//Bootstrap
+	
+	<script src="https://kit.fontawesome.com/9d9796826b.js" crossorigin="anonymous"></script>	
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 
 	<div>
@@ -16,8 +14,7 @@ export default function ShoppingCart() {
 			<a href="#shopping-cart" style="color: mediumblue;">  <i style="margin-right: 1%;" class="fa-solid fa-cart-shopping justify-content-end fa-xl" ></i></a>			 
 		</div>
 		</nav> 
-
-	//Shopping Cart
+	
 	<section class="container content-section" id="shopping-cart">
 		  
 		<h3 style="text-decoration: underline;text-align: center; margin-bottom: 7%;">Shopping Cart</h3>
@@ -43,9 +40,8 @@ export default function ShoppingCart() {
 		<div class="btn-purchase" style="margin-top: 10%;">
 			<button type="button"  class="btn btn-dark btn-purchase "><i class="fa-solid fa-cart-shopping " style="margin-right: 6%;"></i>Buy</button>
 		</div>
-	</section>
-	 
-	//Footer
+	</section>	 
+	
 	<section id="footer">
 		<div class="card text-center">
 		   
