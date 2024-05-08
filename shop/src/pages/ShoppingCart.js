1126722fa633710ec2dfed42f5b49e8454776735
MinaStormcloud <164744381+MinaStormcloud@users.noreply.github.com>
2024-05-08 14:07:56 +0200

    Update ShoppingCart.js

--- shop/src/pages/ShoppingCart.js
+++ shop/src/pages/ShoppingCart.js
@@ -2,26 +2,13 @@ import React from 'react';
 
 export default function ShoppingCart() {
   return (
-    <div>
-		//Fonts
-		<link rel="preconnect" href="https://fonts.googleapis.com">
-		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-		<link href="https://fonts.googleapis.com/css2?family=Inter:wght@200;300;400;500&family=Karla:wght@200;300;400;500&family=Montserrat+Alternates:ital,wght@0,300;0,400;1,300;1,400&family=Montserrat:wght@300;400;500&family=Open+Sans:ital,wght@0,300;0,400;1,300;1,400&display=swap" rel="stylesheet">
-
-		//Bootstrap	
-		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-
-		//Script
-
-		</head>
-
+    	<div>		
+		
 		//Font
 		<script src="https://kit.fontawesome.com/9d9796826b.js" crossorigin="anonymous"></script>
-
-		<body>
+		
 		//Bootstrap
 		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
-
 
 		<div>
 		<nav class="navbar fixed-top bg-light">
@@ -46,8 +33,7 @@ export default function ShoppingCart() {
 			  <th>Quantity</th>
 			</tr>
 		  </thead>
-		  <tbody class="cart-items">
-			
+		  <tbody class="cart-items">			
 			 
 		  </tbody>
 		</table>
@@ -55,13 +41,11 @@ export default function ShoppingCart() {
 			<div class="cart-total"> 
 			  <strong class="cart-total-title">Total</strong>
 			  <span  class="cart-total-price" style="text-align: right;">$0</span>
-			</div>
-		 
+			</div>		 
 
 			<div class="btn-purchase" style="margin-top: 10%;">
 			  <button type="button"  class="btn btn-dark btn-purchase "><i class="fa-solid fa-cart-shopping " style="margin-right: 6%;"></i>Buy</button>
 			</div>
-
 		</section>
 	 
 		//Footer
