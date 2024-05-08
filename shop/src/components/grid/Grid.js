26d7ca4dd3707d0928b9934baad8afc889c0a766
Rickard Sandgren <Rickard.Sandgren@cybercom.com>
2024-05-08 16:46:46 +0200

    put cake json inside the src folder

--- shop/src/components/grid/Grid.js
+++ shop/src/components/grid/Grid.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import './Grid.css'; 
-
+import './Grid.css';
+import data from './../../assets/cake_description.json';
 
 function importAll(r) {
   let images = [];
@@ -12,16 +12,23 @@ function Grid() {
   // Importing images using the importAll function
   const images = importAll(require.context('../../assets/images', false, /\.(png|jpe?g|svg)$/));
 
-  return (
-    <div class="grid">
+  return (<div class="grid gap-4">
+    {data}
+  </div>
+
+  );
+}
+
+
+/*
+ <div class="grid">
       {images.map((imgSrc, index) => (
-        <div key={index} class="grid-item">
+        <div key={index} className="grid-item">
           <img src={imgSrc} alt={`Cake ${index}`} style={{ width: '100%', maxHeight: '300px', objectFit: 'cover' }} />
         </div>
       ))}
     </div>
-  );
-}
+*/
 
 
 
