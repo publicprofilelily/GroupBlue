b370299c1c18862f48711a653a4d3f486d13f962
Rickard Sandgren <Rickard.Sandgren@cybercom.com>
2024-05-09 21:19:05 +0200

    grid design adjustments

--- shop/src/components/grid/Grid.js
+++ shop/src/components/grid/Grid.js
@@ -8,13 +8,15 @@ function Grid() {
     {data.map(cake => (
       <div key={cake.picture} className="grid-item ">
         <div class=" bg-white  duration-500 hover:scale-105 hover:shadow-xl relative">
-          <img src={cake.picture} alt={`Cake ${cake.name}`} class="object-cover rounded-md w-60 shadow-md rounded-md" />
-          <div class="px-4 py-3 font-bold absolute right-[-10px] bg-white p-1 border-[1px] bottom-[-10px]">
-            <p class="text-xl block tracking-wider text-shadow">{cake.price} :-</p>
+          <img src={cake.picture} alt={`Cake ${cake.name}`} class="object-cover rounded-md w-60 shadow-md" />
+          <div class="px-4 py-3 font-bold absolute right-[-15px] bg-yellow-100 p-1 border-[1px] shadow-md bottom-[-10px] text-black border-dashed -rotate-2">
+            <p class="text-md block tracking-wider text-shadow">{cake.price} kr</p>
           </div>
         </div>
         <div class="w-60">
-          <p class="pt-4 pb-[20px] truncate text-lg tracking-wider shadow-pink-950">{cake.name}</p>
+          <p class="pt-6 pb-2 pl-1 truncate text-md tracking-wider font-bold shadow-pink-950 scale-y-110">{cake.name}</p>
+          <p class="pl-1 pb-[20px] truncate text-sm tracking-wider text-gray-400 shadow-pink-950 scale-y-110">{cake.description}</p>
+
         </div>
       </div >
     ))
