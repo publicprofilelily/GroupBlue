e5c0f7a20aa576e54ad0f19915b7fb1cf2af8ecc
Rickard Sandgren <Rickard.Sandgren@cybercom.com>
2024-05-09 13:55:09 +0200

    updated product grid with design, prices and title

--- shop/src/components/grid/Grid.js
+++ shop/src/components/grid/Grid.js
@@ -1,16 +1,25 @@
 import React from 'react';
-import './Grid.css';
-import data from './../../assets/cake_description.json';
+//import './Grid.css';
+import data from './../../data/cakes.json';
 
 function Grid() {
 
-  return <div class="grid gap-4">
+  return <div class="grid grid-cols-4 gap-10">
     {data.map(cake => (
-      <div key={cake.picture} className="grid-item">
-        <img src={cake.picture} alt={`Cake ${cake.name}`} style={{ width: '100%', maxHeight: '300px', objectFit: 'cover' }} />
-      </div>
-    ))}
-  </div>
+      <div key={cake.picture} className="grid-item ">
+        <div class=" bg-white  duration-500 hover:scale-105 hover:shadow-xl relative">
+          <img src={cake.picture} alt={`Cake ${cake.name}`} class="object-cover rounded-md w-60 shadow-md rounded-md" />
+          <div class="px-4 py-3 font-bold absolute right-[-10px] bg-white p-1 border-[1px] bottom-[-10px]">
+            <p class="text-xl block tracking-wider text-shadow">{cake.price} :-</p>
+          </div>
+        </div>
+        <div class="w-60">
+          <p class="pt-4 pb-[20px] truncate text-lg tracking-wider shadow-pink-950">{cake.name}</p>
+        </div>
+      </div >
+    ))
+    }
+  </div >
 }
 
 export default Grid;
--- shop/src/pages/Products.js
+++ shop/src/pages/Products.js
@@ -4,8 +4,8 @@ import Grid from '../components/grid/Grid';
 export default function Products() {
   return (
     <div>
-      <h1>Product Page</h1>
-      <Grid />  
+      <div className="text-xl justify-center flex w-full p-10 text-gray-500 tracking-widest">All Our Wonderful Cakes</div>
+      <Grid />
     </div>
   );
 }
