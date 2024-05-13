94b8d362fecdb1d8ab0c85eaf1b108b1500cff28
LilithyTv <116434991+LilithyTv@users.noreply.github.com>
2024-05-13 13:24:21 +0200

    changes

--- shop/src/Layout.js
+++ shop/src/Layout.js
@@ -1,26 +1,37 @@
 import React from 'react';
 import Header from './components/Header';
+import Sidebar from './components/sidebar/Sidebar'; // Correct import path
 import { Link } from 'react-router-dom';
-
 
 // Pass the child props
 export default function Layout({ children }) {
-	return (
-		<div>
-			<Header>
-				<nav >
-					<ul className="grid grid-flow-col gap-10" >
-						<li><Link to="/">Home</Link></li>
-						<li><Link to="/About">About us</Link></li>
-						<li><Link to="/products">Products</Link></li>
-						<li><Link to="/contact">Contact</Link></li>
-						<li><Link to="/profile">Profile</Link></li>
-					</ul>
-				</nav>
-			</Header>
-			<div className="flex justify-center m-4">
-				{children}
-			</div>
-		</div>
-	);
+  return ( 
+    <div>
+      <Header>
+        <nav>
+          <ul className="grid grid-flow-col gap-10">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/About">About us</Link>
+            </li>
+            <li>
+              <Link to="/products">Products</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+            <li>
+              <Link to="/profile">Profile</Link>
+            </li>
+          </ul>
+        </nav>
+      </Header>
+      <Sidebar /> {/* Sidebar component added here */}
+      <div className="flex justify-center m-4" style={{ marginLeft: "200px" }}>
+        {children}
+      </div>
+    </div>
+  );
 }
---
+++ shop/src/components/sidebar/sidebar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import './Sidebar.css'; 
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      <ul className="menu-items">
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About Us</Link></li>
+        <li><Link to="/contact">Contact</Link></li>
+        <li><Link to="/products">Products</Link></li>
+      </ul>
+    </div>
+  );
+};
+export default Sidebar;
