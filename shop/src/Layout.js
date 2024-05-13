bc9541c3cfc9ecc4c9fb435c6ffb17eb44ae8221
LilithyTv <116434991+LilithyTv@users.noreply.github.com>
2024-05-13 14:17:16 +0200

    update layout

--- shop/src/Layout.js
+++ shop/src/Layout.js
@@ -1,26 +1,25 @@
 import React from 'react';
 import Header from './components/Header';
+import Sidebar from './components/sidebar/Sidebar'; // Ensure this path matches the actual file location
 import { Link } from 'react-router-dom';
-
 
 // Pass the child props
 export default function Layout({ children }) {
-	return (
-		<div>
-			<Header>
-				<nav >
-					<ul className="grid grid-flow-col gap-10" >
-						<li><Link to="/">Home</Link></li>
-						<li><Link to="/About">About us</Link></li>
-						<li><Link to="/products">Products</Link></li>
-						<li><Link to="/contact">Contact</Link></li>
-						<li><Link to="/profile">Profile</Link></li>
-					</ul>
-				</nav>
-			</Header>
-			<div className="flex justify-center m-4">
-				{children}
-			</div>
-		</div>
-	);
+  return ( // Added the return statement here
+    <div>
+      <Header>
+        <nav>
+          <ul className="grid grid-flow-col gap-10">
+            <li>
+              <Link to="/profile">Profile</Link>
+            </li>
+          </ul>
+        </nav>
+      </Header>
+      <Sidebar /> {/* Sidebar component added here */}
+      <div className="flex justify-center m-4" style={{ marginLeft: "200px" }}>
+        {children}
+      </div>
+    </div>
+  );
 }
